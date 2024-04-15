38e459ec15f86853151e27ed97e38c00a69dbe7d
q4w3e2r1 <126683181+q4w3e2r1@users.noreply.github.com>
2024-04-15 16:41:23 +0500

    Update 4.ts

--- курс/8 тема/4.ts
+++ курс/8 тема/4.ts
@@ -1,38 +1,38 @@
-// type X = { 
-//     x: { 
-//       a: 1
-//       b: 'hi'
-//           z: string
-//     }
-//     y: string
-//   }
+type X = { 
+    x: { 
+      a: 1
+      b: 'hi'
+          z: string
+    }
+    y: string
+  }
   
   
-//   type Expected = { 
-//     readonly x: { 
-//       readonly a: 1
-//       readonly b: 'hi'
-//     }
-//     readonly y: 'hey' 
-//   }
+  type Expected = { 
+    readonly x: { 
+      readonly a: 1
+      readonly b: 'hi'
+    }
+    readonly y: 'hey' 
+  }
   
   
-//   type Todo = DeepReadonly<X> // should be same as `Expected`
+  type Todo = DeepReadonly<X> // should be same as `Expected`
   
   
-//   const test: Todo = {
-//       x: {
-//           a: 1,
-//           b: 'hi',
-//           z: 'try change me too',
-//       },
-//       y: 'try change me',
-//   };
+  const test: Todo = {
+      x: {
+          a: 1,
+          b: 'hi',
+          z: 'try change me too',
+      },
+      y: 'try change me',
+  };
   
   
-//   test.y = 'changed'; // Error
-//   test.x.z = 'changed'; // Error
+  test.y = 'changed'; // Error
+  test.x.z = 'changed'; // Error
 
-//   type DeepReadonly<T> = {
-//     readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P]
-//   }
+  type DeepReadonly<T> = {
+    readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P]
+  }
