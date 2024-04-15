8c8fdd1f7635a1c31dfabd11fcced921ea7a1489
q4w3e2r1 <126683181+q4w3e2r1@users.noreply.github.com>
2024-04-15 14:49:46 +0500

    Create 6.ts

---
+++ курс/4 тема/6.ts
@@ -0,0 +1,26 @@
+//6
+export type User = {
+    name:string;
+    age:number;
+    occupation: string;
+};
+
+export const users: User[] = [
+    {
+        name: 'Roman Abramov',
+        age: 25,
+        occupation: 'Millionaire'
+    },
+    {
+        name: 'Andrey Fox',
+        age: 23,
+        occupation: 'Developer'
+    }
+];
+
+export function logPerson(user: User) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log('Users:');
+users.forEach(logPerson);
