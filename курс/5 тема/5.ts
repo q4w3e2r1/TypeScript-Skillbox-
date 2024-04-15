7d422e43497768e7d076c95e4c85e9c81a7d7ec8
q4w3e2r1 <126683181+q4w3e2r1@users.noreply.github.com>
2024-04-15 14:52:10 +0500

    Create 5.ts

---
+++ курс/5 тема/5.ts
@@ -0,0 +1,51 @@
+//5
+type User = {
+    type: 'user';
+    name: string;
+    age: number;
+    occupation: string;
+}
+
+type Admin = {
+    type: 'admin';
+    name: string;
+    age: number;
+    role: string;
+}
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    { type: 'user', name: 'Max Mustermann', age: 25, occupation: 'Chimney sweep' },
+    { type: 'admin', name: 'Jane Doe', age: 32, role: 'Administrator' },
+    { type: 'user', name: 'Kate Müller', age: 23, occupation: 'Astronaut' },
+    { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
+];
+
+// после добавления 'person is Admin' и соотвествующего для User, мы дали понять компилятору, что эта фукнция определяет явялется ли объект данным типом
+export function isAdmin(person: Person): person is Admin {
+    return person.type === 'admin';
+}
+
+export function isUser(person: Person): person is User {
+    return person.type === 'user';
+}
+
+export function logPerson(person: Person) {
+    let additionalInformation: string = '';
+    if (isAdmin(person)) {
+        additionalInformation = person.role;
+    }
+    if (isUser(person)) {
+        additionalInformation = person.occupation;
+    }
+    console.log(` - ${person.name}, ${person.age}, ${additionalInformation}`);
+}
+
+console.log('Admins:');
+persons.filter(isAdmin).forEach(logPerson);
+
+console.log();
+
+console.log('Users:');
+persons.filter(isUser).forEach(logPerson);
