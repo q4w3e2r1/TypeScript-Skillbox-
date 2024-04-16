06320a6634df9b48c0a81c8783c1f65e89ad2994
q4w3e2r1 <126683181+q4w3e2r1@users.noreply.github.com>
2024-04-16 12:53:11 +0500

    Fix

--- курс/9 тема/src/checkEmail.ts
+++ курс/9 тема/src/checkEmail.ts
@@ -1,5 +1,8 @@
 import { validate } from "email-validator";
 
 export function CheckEmail(email: string): boolean{
+    if(!isNaN(Number(email))){
+        return false
+    }
     return validate(email)
 }
